0769691137e9986dd7f4913881990a429314c99f
= <=>
2025-04-02 15:21:39 -0400

    Added Jasmine tests for About Us, Settings Menu, Side Menu, Tip Info, and Tip Procedure components

--- frontend/src/app/components/side-menu/side-menu.component.spec.ts
+++ frontend/src/app/components/side-menu/side-menu.component.spec.ts
@@ -1,24 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { SideMenuComponent } from './side-menu.component';
 import { provideIonicAngular } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { globe, call, happy, menu } from 'ionicons/icons';
+import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SideMenuComponent', () => {
   let component: SideMenuComponent;
   let fixture: ComponentFixture<SideMenuComponent>;
+  let menuControllerSpy: jasmine.SpyObj<MenuController>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(waitForAsync(() => {
-    addIcons({
-      globe,
-      call,
-      happy,
-      menu,
-    });
+    // Create spies for the menuController and router
+    menuControllerSpy = jasmine.createSpyObj('MenuController', ['close']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
-      imports: [SideMenuComponent],
-      providers: [provideIonicAngular()],
+      imports: [SideMenuComponent, RouterTestingModule],
+      providers: [
+        { provide: MenuController, useValue: menuControllerSpy },
+        { provide: Router, useValue: routerSpy },
+        provideIonicAngular(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SideMenuComponent);
@@ -28,5 +32,49 @@ describe('SideMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call router.navigate() when goToSettings is called', async () => {
+    // Call the goToSettings method
+    await component.goToSettings();
+
+    // Assert that router.navigate was called with the correct argument
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/settings']);
+  });
+
+  it('should have ion-menu element', () => {
+    const menuElement = fixture.nativeElement.querySelector('ion-menu');
+
+    // Assert that the ion-menu element exists
+    expect(menuElement).toBeTruthy();
+  });
+
+  it('should have ion-list inside ion-content', () => {
+    const ionContent = fixture.nativeElement.querySelector('ion-content');
+    const ionList = ionContent.querySelector('ion-list');
+
+    // Assert that ion-list is inside ion-content
+    expect(ionList).toBeTruthy();
+  });
+
+  it('should have ion-item inside ion-list', () => {
+    const ionList = fixture.nativeElement.querySelector('ion-list');
+    const ionItem = ionList.querySelector('ion-item');
+
+    // Assert that ion-item is present inside ion-list
+    expect(ionItem).toBeTruthy();
+  });
+
+  it('should trigger goToSettings() when ion-item is clicked', async () => {
+    const ionItem = fixture.nativeElement.querySelector('ion-item');
+
+    // Spy on goToSettings method
+    spyOn(component, 'goToSettings').and.callThrough();
+
+    // Simulate click on ion-item
+    ionItem.click();
+
+    // Assert that goToSettings was called when ion-item is clicked
+    expect(component.goToSettings).toHaveBeenCalled();
   });
 });
