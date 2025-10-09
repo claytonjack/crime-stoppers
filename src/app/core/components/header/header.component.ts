64c269c42805752a691c7d7c5ce23d655aa3a208
= <=>
2025-10-09 19:02:41 -0400

    Implemented Screen Reader features on Header and Side-menu Components

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 import {
   IonHeader,
@@ -8,6 +8,7 @@ import {
   IonButton,
   IonMenuToggle,
 } from '@ionic/angular/standalone';
+import { ScreenReaderService } from '@app/core/pages/settings/services/screen-reader.service';
 
 @Component({
   selector: 'app-header',
@@ -27,4 +28,18 @@ import {
 export class HeaderComponent {
   @Input() title: string = '';
   @Input() showMenuButton: boolean = true;
+
+   private readonly screenReader = inject(ScreenReaderService);
+
+  async ngOnInit() {
+    // Announce the current page title when the header loads
+    if (this.title) {
+      await this.screenReader.speak(`${this.title} page loaded`);
+    }
+  }
+
+  async onMenuButtonClick() {
+    // Announce menu button activation
+    await this.screenReader.speak('Opening side menu');
+  }
 }
